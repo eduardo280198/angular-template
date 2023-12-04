f85520134743a8945a347b406cf031b8cf9a0733
Eduardo Lozano Alvarado <eduardo_280198@hotmail.com>
2023-12-04 11:25:39 -0600

    Terminando el Form-masking

--- src/app/demo/forms/form-masking/form-masking.component.ts
+++ src/app/demo/forms/form-masking/form-masking.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+// Dependencias
+import {NgxMaskDirective, provideNgxMask} from "ngx-mask";
+import {IConfig} from "ngx-mask";
 
+import { Component } from '@angular/core';
+import {CardModule} from "../../../theme/shared/components";
+
+// Configuracion del mask.
+const maskConfig : Partial<IConfig> = {
+
+}
 
 @Component({
   selector: 'app-form-masking',
   standalone: true,
-  imports: [],
   templateUrl: './form-masking.component.html',
-  styleUrls: ['./form-masking.component.scss']
+  styleUrls: ['./form-masking.component.scss'],
+  imports: [NgxMaskDirective, CardModule,],
+  providers: [provideNgxMask(maskConfig),]
 })
 export class FormMaskingComponent {
 
