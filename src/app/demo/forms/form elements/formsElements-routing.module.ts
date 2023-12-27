0df4da6a29330205bfd199138b5ac0acfea6b0d5
Eduardo Lozano Alvarado <eduardo_280198@hotmail.com>
2023-12-27 17:01:52 -0600

    Agregando las rutas de angular para formsElements

---
+++ src/app/demo/forms/form elements/formsElements-routing.module.ts
@@ -0,0 +1,30 @@
+import {Route} from "@angular/router";
+
+export const FORMS_ELEMENTS_ROUTES : Route[] = [
+
+  {
+    path: 'basic',
+    loadComponent: () => import('./form-basic/form-basic.component').then(m => m.FormBasicComponent ),
+  },
+  {
+    path: 'advance',
+    loadComponent: () => import('./form-advance/form-advance.component').then(m => m.FormAdvanceComponent ),
+  },
+  {
+    path: 'validation',
+    loadComponent: () => import('./form-validation/form-validation.component').then(m => m.FormValidationComponent )
+  },
+  {
+    path: 'masking',
+    loadComponent: () => import('./form-masking/form-masking.component').then(m => m.FormMaskingComponent ),
+  },
+  {
+    path: 'picker',
+    loadComponent: () => import('./form-picker/form-picker.component').then(m => m.FormPickerComponent ),
+  },
+  {
+    path: 'select',
+    loadComponent: () => import('./form-select/form-select.component').then(m => m.FormSelectComponent )
+  }
+
+]
